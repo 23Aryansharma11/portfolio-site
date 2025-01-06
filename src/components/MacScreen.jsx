c65315f189552ab9e2c3c32409f115365be89244
Aryan Sharma <23aryansharma11@gmail.com>
2025-01-06 09:12:37 +0530

    add lazy loading images

--- src/components/MacScreen.jsx
+++ src/components/MacScreen.jsx
@@ -15,7 +15,7 @@ const MacScreen = ({ projectInfo }) => {
         <div
           className="flex items-center justify-center bg-cover bg-center w-full h-auto relative overflow-hidden text-white-100 dark:bg-dark-300"
         >
-          <img src={imageUrl} className="w-full h-full object-contain" alt="" />
+          <img src={imageUrl} className="w-full h-full object-contain" alt="" loading="lazy" />
           <div className='w-full h-full bg-black/80 absolute flex flex-col justify-center gap-10 items-center z-10 opacity-0 transition-opacity duration-300 hover:opacity-100 text-center'>
             <h4 className='text-white capitalize text-lg md:text-xl lg:text-2xl font-medium dark:text-white'>{projectName}</h4>
             <p className='text-white text-xs md:text-sm dark:text-gray-300 text-balance'>{projectDescription}</p>
